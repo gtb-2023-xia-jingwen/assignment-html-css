8f99381b612f931dc1771fe7a68fa0bbb9960761
Li <lzhou@thoughtworks.com>
2022-04-13 17:39:54 +0800

    update test snapshot

--- cypress/integration/todos.test.js
+++ cypress/integration/todos.test.js
@@ -3,22 +3,15 @@ describe("Story 1: TODO list layout", () => {
     cy.visit("/");
   });
 
-  it("should match snapshot", () => {
-    cy.matchImageSnapshot("todo-list-default", {
-      failureThreshold: 0.03,
-      failureThresholdType: "percent",
-      customDiffConfig: { threshold: 0.1 },
-    });
-  });
-
   it("should match snapshot when hover on todo item", () => {
-    cy.get(".todo-lists li:first .delete-btn")
+    cy.get("ul.todo-lists li:first .delete-btn")
       .should("be.hidden")
       .invoke("show");
     cy.matchImageSnapshot("todo-item-hover", {
-      failureThreshold: 0.03,
+      failureThreshold: 0.02,
       failureThresholdType: "percent",
       customDiffConfig: { threshold: 0.1 },
+      capture: "viewport",
     });
   });
 });
